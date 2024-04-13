be18e31f9941e72c56e2256275b2c70d5be4a577
Sujash Nayak <45498613+SNubby19@users.noreply.github.com>
2024-04-13 09:07:21 -0400

    Update firebase.ts
    sujash did an oopsie that needed to be fixed

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,13 +4,7 @@ import { getFirestore } from "firebase/firestore";
 import { getFunctions } from "firebase/functions";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBKmtfqiXWG1bSETEVxW9zvv08bKBcnCRc",
-  authDomain: "uinsports-mobile-app.firebaseapp.com",
-  projectId: "uinsports-mobile-app",
-  storageBucket: "uinsports-mobile-app.appspot.com",
-  messagingSenderId: "1039521033465",
-  appId: "1:1039521033465:web:5a95cce0d439a269128628",
-  measurementId: "G-WQDP9MNYPD",
+  //no looking
 };
 
 const app = initializeApp(firebaseConfig);
